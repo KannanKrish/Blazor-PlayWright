d8f0bd975e018f6ee74b1baac3c9a35b56f8a9b9
Kannan Chithambaranathan <ckannan759153@gmail.com>
2023-01-05 22:17:50 +0530

    Add project files.

---
+++ BlazorTest.cs
@@ -0,0 +1,67 @@
+using Microsoft.Playwright;
+using Microsoft.Playwright.MSTest;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PlaywrightTests;
+
+[TestClass]
+public class BlazorTest : PageTest
+{
+    private static WebApplication _app = null!;
+
+    [AssemblyInitialize]
+    public static void AssemblyInitialize2(TestContext context)
+    {
+        Console.Error.WriteLine("AssemblyInitialize");
+        var baseDir = Path.Combine(AppContext.BaseDirectory, "..", "..", "..");
+        var builder = WebApplication.CreateBuilder(new WebApplicationOptions
+        {
+            EnvironmentName = "Development",
+            ContentRootPath = baseDir,
+            WebRootPath = Path.Combine(baseDir, "wwwroot"),
+            ApplicationName = "BlazorApp",
+        });
+
+        // Add services to the container.
+        builder.Services.AddRazorPages();
+        builder.Services.AddServerSideBlazor();
+        builder.Services.AddSingleton<WeatherForecastService>();
+
+        _app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!_app.Environment.IsDevelopment())
+        {
+            _app.UseExceptionHandler("/Error");
+        }
+        
+        _app.UseStaticFiles();
+
+        _app.UseRouting();
+
+        _app.MapBlazorHub();
+        _app.MapFallbackToPage("/_Host");
+
+        var readyTcs = new CancellationTokenSource();
+        
+        _ = Task.Run(async () =>
+        {
+            await _app.StartAsync(readyTcs.Token);
+            readyTcs.Cancel();
+        }).ConfigureAwait(false);
+        
+        readyTcs.Token.WaitHandle.WaitOne();
+    }
+
+    [AssemblyCleanup]
+    public static async Task AssemblyCleanup() => await _app.StopAsync();
+
+    public override BrowserNewContextOptions ContextOptions()
+    {
+        var options = base.ContextOptions() ?? new BrowserNewContextOptions();
+
+        options.BaseURL = "http://localhost:5000";
+
+        return options;
+    }
+}
